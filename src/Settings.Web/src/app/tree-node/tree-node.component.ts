a2e20d48ced2de5ab3cf571858b32b231a193c23
CJ Taylor <cjtayl05@gmail.com>
2017-09-17 20:53:44 -0400

    frontend; tests; fixed e2e and reintroduced to scripts/test.

--- src/Settings.Web/src/app/tree-node/tree-node.component.ts
+++ src/Settings.Web/src/app/tree-node/tree-node.component.ts
@@ -4,7 +4,7 @@ import { TreeNode } from '../treenode';
 
 
 @Component({
-  selector: 'app-tree-node',
+  selector: 'tree-node',
   templateUrl: './tree-node.component.html',
   styleUrls: ['./tree-node.component.scss']
 })
